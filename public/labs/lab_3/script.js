8ff6b588f0b148d717acfa968bf5751e5b7df4e5
Samar Majid <38479444+samarmajid@users.noreply.github.com>
2021-02-12 21:48:27 -0500

    lab 3 work

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,29 @@
-/* Put your javascript in here */
+ const list_container = document.querySelector(".images")
+
+ function shift_left(){
+     const last_three = Array.from(list_container.children).slice(4,7).reverse();
+     last_three.forEach((element) => {
+         list_container.removeChild(element);
+         list_container.insertBefore(element, list_container.children[0]);
+     });
+ }
+
+ function shift_right(){
+    const first_three = Array.from(list_container.children).slice(0,3);
+    first_three.forEach((element) => {
+        list_container.removeChild(element);
+        list_container.appendChild(element);
+    });
+}
+
+function load_page(){
+    document.querySelector("button.arrow.prev").addEventListener("click", (event) => {
+        shift_left();
+    });
+    document.querySelector("button.arrow.next").addEventListener("click", (event) => {
+        shift_right()
+    });
+   
+}
+window.onload = load_page;
+
